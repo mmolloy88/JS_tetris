d61ca80a2349b87a8e63232e629e52738c4b47d5
mmolloy <mmolloy88@outlook.com>
2020-06-04 23:39:39 +1000

    core game is working

--- game.js
+++ game.js
@@ -2,9 +2,12 @@ document.addEventListener('DOMContentLoaded', () =>{
 
     const grid = document.querySelector('.grid')
     let squares = Array.from(document.querySelectorAll('.grid div'))
-    const ScoreDisplay = document.querySelector('#score')
-    const StartBtn = document.querySelector('#start-button')
+    const scoreDisplay = document.querySelector('#score')
+    const startBtn = document.querySelector('#start-button')
     const width = 10
+    let nextRandom = 0
+    let timerId
+    let score = 0
 
  // tetrominoes
     
@@ -69,7 +72,7 @@ document.addEventListener('DOMContentLoaded', () =>{
         }
 
     //move down timer
-    timerId = setInterval(moveDown, 750)
+    //timerId = setInterval(moveDown, 750)
 
     // assign functions to keyCodes
     function control(e) {
@@ -98,10 +101,14 @@ document.addEventListener('DOMContentLoaded', () =>{
         if(current.some(index => squares[currentPosition + index + width].classList.contains("taken"))) {
             current.forEach(index => squares[currentPosition + index].classList.add("taken"))
             // start new tetromino falling
-            random = Math.floor(Math.random()*theTetrominoes.length)
+            random = nextRandom
+            nextRandom = Math.floor(Math.random()*theTetrominoes.length)
             current = theTetrominoes[random][currentRotation]
             currentPosition = 4
             draw()
+            displayShape()
+            addScore()
+            gameOver()
         }
     }
 
@@ -146,6 +153,86 @@ document.addEventListener('DOMContentLoaded', () =>{
 
 
     // show the next tetromino in minigrid
-    const 
+    const displaySquares = document.querySelectorAll('.mini-grid div')
+    const displayWidth = 4
+    let displayIndex = 0
+
+    // next tertomino without rotation
+    const upNextTetrominoes = [
+        [1, displayWidth+1, displayWidth*2+1, 2], //lTertomino
+        [0, displayWidth, displayWidth+1, displayWidth*2+1], //zTertomino
+        [1, displayWidth, displayWidth+1, displayWidth+2], //tTetromino
+        [0, 1, displayWidth, displayWidth+1], //oTetromino
+        [1, displayWidth+1, displayWidth*2+1, displayWidth*3+1] //iTetromino
+    ]
+
+    //display the shape in the minigrid
+    function displayShape() {
+        displaySquares.forEach(square => {
+            square.classList.remove('tetromino')
+        })
+        upNextTetrominoes[nextRandom].forEach( index => {
+            displaySquares[displayIndex +index].classList.add('tetromino')
+        })
+    }
+
+
+    // add functionality to the start/pause button
+    startBtn.addEventListener('click', () => {
+        if (timerId) {
+            clearInterval(timerId)
+            timerId = null
+        } else {
+            draw()
+            timerId = setInterval(moveDown, 750)
+            nextRandom = Math.floor(Math.random()*theTetrominoes.length)
+            displayShape()
+        }
+    })
+
+    // add score
+    function addScore() {
+        for (let i = 0; i < 199; i +=width) {
+            const row = [i, i+1, i+2, i+3, i+4, i+5, i+6, i+7, i+8, i+9]
+        
+            if(row.every(index => squares[index].classList.contains('taken'))) {
+                score +=10
+                scoreDisplay.innerHTML = score
+                row.forEach(index => {
+                    squares[index].classList.remove('taken')
+                    squares[index].classList.remove('tetromino')
+                })
+                const squaresRemoved = squares.splice(i, width)
+                squares = squaresRemoved.concat(squares)
+                squares.forEach(cell => grid.appendChild(cell))
+            }
+        }
+    }
+
+    // game over
+    function gameOver() {
+        if(current.some(index => squares[currentPosition + index].classList.contains('taken'))) {
+            scoreDisplay.innerHTML = 'end'
+            clearInterval(timerId)
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 })
